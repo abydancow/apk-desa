c0f6dee523290fcb5a7a6a2d3d28e4573c0cfde7
aby <syafrillahastro@gmail.com>
2024-10-17 04:03:02 +0700

    perubahan kecil

--- page/admin.py
+++ page/admin.py
@@ -1,28 +1,19 @@
-import os
-import kivy
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
-from kivy.config import Config
-from kivy.lang import Builder
-from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
 
-Config.set('graphics', 'width', '360')
-Config.set('graphics', 'height', '640')
-Config.set('graphics', 'resizable', False) 
+# Config.set('graphics', 'width', '360')
+# Config.set('graphics', 'height', '640')
+# Config.set('graphics', 'resizable', False) 
 
 
-class MyGrid(Widget): 
+class AdminScreen(Screen): 
     pass
 
 
-class AdminApp(App):
-    def build(self):
-        kv_file_path = os.path.join(os.path.dirname(__file__), '..', 'kivy', 'admin.kv')
-        Builder.load_file(kv_file_path)
-        return MyGrid()
+# class AdminApp(App):
+#     def build(self):
+#         kv_file_path = os.path.join(os.path.dirname(__file__), '..', 'kivy', 'admin.kv')
+#         Builder.load_file(kv_file_path)
+#         return MyGrid()
     
-if __name__ =="__main__":
-    AdminApp() .run()
+# # if __name__ =="__main__":
+#     AdminApp() .run()
